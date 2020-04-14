6c03d97c76642f41955670752d4c0ab6469ab9ce
Naomi <naomixnemo@gmail.com>
2020-04-14 16:50:24 -0400

    Mass update

--- module.js
+++ module.js
@@ -1,1 +1,8 @@
-
+const nFX = require ("./helper.js");
+exports.main = (path, options = undefined) => {
+    var _this = { errors: [] };
+    if (typeof path != "string" || !path) throw new Error ("path is not a string");
+    nFX.lib.func.path.main.call (_this, path, options); if (_this.errors[0]) throw _this.errors[0];
+    var _type = nFX.lib.func.typeof (nFX.node_modules.fs.lstatSync (_this.path[0]));
+    return new _type (path, options);
+};
